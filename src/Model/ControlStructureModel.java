dfcdd87f1fc15b854afc6895c0254301aab4282f
psbandarigoda <37591476+psbandarigoda@users.noreply.github.com>
2020-05-09 17:37:24 +0530

    control structure model added

---
+++ src/Model/ControlStructureModel.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Dakshika
+ */
+public class ControlStructureModel {
+    
+    String line;
+    String wtcs;
+    String NC;
+    String Ccspps;
+    String Ccs;
+    
+    public ControlStructureModel() {
+    }
+
+    public ControlStructureModel(String line, String wtcs, String NC, String ccspps, String ccs) {
+        this.line = line;
+        this.wtcs = wtcs;
+        this.NC = NC;
+        Ccspps = ccspps;
+        Ccs = ccs;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public String getWtcs() {
+        return wtcs;
+    }
+
+    public void setWtcs(String wtcs) {
+        this.wtcs = wtcs;
+    }
+
+    public String getNC() {
+        return NC;
+    }
+
+    public void setNC(String NC) {
+        this.NC = NC;
+    }
+
+    public String getCcspps() {
+        return Ccspps;
+    }
+
+    public void setCcspps(String ccspps) {
+        Ccspps = ccspps;
+    }
+
+    public String getCcs() {
+        return Ccs;
+    }
+
+    public void setCcs(String ccs) {
+        Ccs = ccs;
+    }
+    
+}
